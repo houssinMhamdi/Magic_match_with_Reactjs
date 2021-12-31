d80f184201330e570b1c733573902a495a1213cf
Your Name <you@example.com>
2021-12-31 14:50:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  {"src":"/img/helmet-1.png", matched:false},
+  {"src":"/img/potion-1.png", matched:false},
+  {"src":"/img/ring-1.png", matched:false},
+  {"src":"/img/scroll-1.png", matched:false},
+  {"src":"/img/shield-1.png", matched:false},
+  {"src":"/img/sword-1.png", matched:false},
+  
+]
 
 function App() {
+  const [cards,setCards] = useState([])
+  const [turns,setTurns]=useState(0)
+  const [choiceOne,setChoiceOne]=useState(null)
+  const [choiceTwo,setChoiceTwo]=useState(null)
+  const [disabled , setDisabled]=useState(false)
+  
+  const shuffleCards = ()=>{
+  const shuffleCards = [...cardImages , ...cardImages]
+  .sort(()=>Math.random()-0.5)
+  .map((cards)=>({...cards, id:Math.random()}))
+
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setCards(shuffleCards)
+  setTurns(0)
+  
+}
+// handle choice
+const handleChoice=(card)=>{
+  choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+}
+//compair 2 selected cards
+useEffect(()=>{
+  if (choiceOne && choiceTwo) {
+    setDisabled(true)
+    if (choiceOne.src===choiceTwo.src) {
+      setCards(prevCards =>{
+        return prevCards.map(card => {
+          if (card.src === choiceOne.src) {
+             return {...card , matched:true} 
+          }else{
+            return card
+          }
+        })
+      })
+      resetTurns()
+    }else{
+      console.log('not the same cards');
+      setTimeout(()=>resetTurns(),1000)
+    }
+  }
+},[choiceOne,choiceTwo])
+console.log(cards);
+
+
+
+
+//reset choice and increse turns
+
+const resetTurns = () =>{
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setTurns(prevTurns => prevTurns + 1 )
+  setDisabled(false)
+}
+
+useEffect(() => {
+  shuffleCards()
+},[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Magic Match</h1>
+        <button onClick={shuffleCards}>New Game</button>
+        <div className='card-grid'>
+          {cards.map((card)=>(
+            <SingleCard  
+            card={card}
+            key={card.id} 
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}  
+            disabled={disabled}
+            />            
+          ))}
+        </div>
+        <p>Turns: {turns}</p>
     </div>
   );
 }
